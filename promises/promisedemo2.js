643db53e45ddf7a1fc882b4267a04c030d47b5b6
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-03-24 17:21:10 +0530

    implemented a promise object

---
+++ promises/promisedemo2.js
@@ -0,0 +1,18 @@
+const p = new Promise(function exec(res,rej){
+    console.log("Executor callback triggered by Promise constructor");
+    setTimeout(()=>{
+        const randomNumber = Math.floor(Math.random()*100);
+        console.log(randomNumber);
+        if(randomNumber % 2 === 0){
+            //random number is even
+            res();
+        }
+        else{
+            //random Number is odd
+            rej();
+        }
+    },5000);
+
+});
+console.log("Created promise object ");
+console.log(p);
