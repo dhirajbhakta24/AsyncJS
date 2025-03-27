93a2736618be7419ff58622c639b9d378979caa1
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-03-28 01:06:43 +0530

    added generator codes

---
+++ iterator_generator/generator.demo3.js
@@ -0,0 +1,13 @@
+function* gen(){
+    console.log("inside generator");
+    const x = yield 10;
+    const y = x + 30;
+    yield y;
+}
+
+const it = gen();
+console.log(it.next()); //{value :10 ,done:false}
+//some logic
+console.log(it.next(11));//this line will  inject 11 at line 3 the place where 
+//last yield was called -> x= 11
+
