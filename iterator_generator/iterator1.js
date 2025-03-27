bfefbbe2eddae8ef9e10763a7216657a0e333cc1
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-03-28 00:25:37 +0530

    added custom iterator

---
+++ iterator_generator/iterator1.js
@@ -0,0 +1,20 @@
+/*
+
+ITERATOR
+___________________________________________________________________________
+it is a general programming concept that are present in many languages
+
+it is nothing but a design pattern 
+
+programming language -> 
+1. declarative 
++ we define what to do and dont care about the how 
++ ex: react , Mysql , mongoDb
+
+2.imperative
++ we manually define how to acheive something 
+
+
+NOTE: iterative design pattern help in declarative programming 
+
+**/
